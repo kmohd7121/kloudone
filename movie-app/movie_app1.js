6a026bbb07612000a65aff1f0918b3f3e6cfadf4
kmohd7121 <65274900+kmohd7121@users.noreply.github.com>
2020-11-16 14:02:19 +0530

    Add files via upload

--- movie-app/movie_app1.js
+++ movie-app/movie_app1.js
@@ -3,7 +3,7 @@ const INITIAL_SEARCH_VALUE = 'spiderman';
 const log = console.log;
 
 // Selecting elements from the DOM
-const searchButton = document.querySelector('#search');;
+const searchButton = document.querySelector('#search');
 const searchInput = document.querySelector('#exampleInputEmail1');
 const moviesContainer = document.querySelector('#movies-container');
 const moviesSearchable = document.querySelector('#movies-searchable');
--- movie-app/movie_app2.js
+++ movie-app/movie_app2.js
@@ -1,5 +1,5 @@
 // Initial Values
-const MOVIE_DB_API = 'd8bf019d0cca372bd804735f172f67e8';
+const MOVIE_DB_API = '5b3ba2216ba16f20a8333c77f4aacbc2';
 const MOVIE_DB_ENDPOINT = 'https://api.themoviedb.org';
 const MOVIE_DB_IMAGE_ENDPOINT = 'https://image.tmdb.org/t/p/w500';
 const DEFAULT_POST_IMAGE = 'https://via.placeholder.com/150';
